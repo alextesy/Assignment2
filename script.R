cbecc807735a356847312357f090fc552eb3e961
Alexander Kremyanskyy <ALEXKREM@ibm.com>
2018-04-21 15:31:25 +0300

    Na and training and testing

--- script.R
+++ script.R
@@ -1,8 +1,13 @@
 install.packages("caret")
 install.packages("readxl")
+install.packages("zoo")
 
+library(zoo)
 library("caret")
 library('readxl')
+
+
+#Turn the data into a DATAFRAME
 GermanCredit <- read_xlsx("GermanCredit.xlsx", col_names=FALSE)
 df<-data.frame(as.vector(strsplit(GermanCredit$X__2[1], ","))[[1]])
 for (i in 2:13) {
@@ -13,9 +18,32 @@ for (i in 1:13) {
   colnames(df)[i]<-GermanCredit$X__1[i]
 }
 df[apply(df, 2, function(x) x=="")] = NA
+for (i in 1:13) {
+  if(!is.na(as.numeric(as.character(df[1,i]))))
+  {
+    df[,i]<-as.numeric(as.character(df[,i]))
+  }
+}
+
+#Function for Replacing NA
+  myFun <- function(x) {
+    if (is.numeric(x)) {
+      x[is.na(x)] <- mean(x, na.rm = TRUE)
+      x
+    } else {
+      x[is.na(x)] <- names(which.max(table(x)))
+      x
+    }
+  }
+
+
+
+#Replacint NA and Dividing into Training and Testing Set
+df2 <-as.data.frame(lapply(df, myFun))
 inTrain<-createDataPartition(y=df$class,p=0.8 ,list=FALSE)
 training<-df[inTrain,]
 testing<-df[-inTrain,]
-index=c(1,2,5,6,9,12)
-for(i in length(index))
-  df[,index[i]]=as.numeric(as.character(df[,index[i]]))
+
+
+
+
