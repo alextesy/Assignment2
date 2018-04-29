88de0814c249884a77545962d03981c608ff537b
Alex Kremiansky <alextesy@gmail.com>
2018-04-29 18:29:23 +0300

    DISCRETIZATION AND MODEL

--- script.R
+++ script.R
@@ -1,10 +1,15 @@
 install.packages("caret")
 install.packages("readxl")
 install.packages("zoo")
+install.packages("arules")
+install.packages("rpart")
 
-library(zoo)
+
+library("zoo")
 library("caret")
 library('readxl')
+library('arules')
+library('rpart')
 
 
 #Turn the data into a DATAFRAME
@@ -38,12 +43,33 @@ for (i in 1:13) {
 
 
 
+
 #Replacint NA and Dividing into Training and Testing Set
 df2 <-as.data.frame(lapply(df, myFun))
-inTrain<-createDataPartition(y=df$class,p=0.8 ,list=FALSE)
-training<-df[inTrain,]
-testing<-df[-inTrain,]
+inTrain<-createDataPartition(y=df2$class,p=0.8 ,list=FALSE)
+training<-df2[inTrain,]x
+testing<-df2[-inTrain,]
 
 
 
+#Discretization
+df2$Average_Credit_Balance <- discretize(df2$Average_Credit_Balance,breaks = 3,labels = c("Low","Medium","High"))
+df2$over_draft <- discretize(df2$over_draft,breaks = 3,labels = c("Low","Medium","High"))
+i=0
+for(x in df2$cc_age){
+  i=i+1
+  if(is.na(x)==TRUE){
+    next
+  }else if(x<=30){
+    df2[i,"cc_age"]="Young"
+  } else if(x>30 & x<=60){
+    df2[i,"cc_age"]="Middle"
+  } else{
+    df2[i,"cc_age"]="Old"
+  }
+}
+df2$cc_age<-as.factor(df2$cc_age)
+
+#MODEL
+tree=rpart(class~.,training,method = "class",parms = list(split = "information"),control = list(minsplit=200)  )
 
